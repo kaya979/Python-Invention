8865926b832123cccbb3f56a18e96148f342e50b
kaya karakaya <kaya.md@gmail.com>
2023-08-20 17:47:55 +0200

    added more forloops

--- 04.Looping.for.Example01.py
+++ 04.Looping.for.Example01.py
@@ -28,4 +28,36 @@ sum_nums=0
 for num in nums:
 	sum_nums = sum_nums + num
 
-print(f'Sum of numbers is{sum_nums}')
+print(f'Sum of numbers is {sum_nums}')
+
+
+# the for loop from line 28 wants to iterate/loop for every "num" or item
+# in list () nums and what does it iterate : 
+# it iterates the sum_nums which start at 0, and for every item it 
+# adds num to sum_nums so e.g; sum_nums = 0 +1 = 1
+# the next iteration is sum_nums= 1+2 = 3
+# sum_nums = 3 + 3 = 6
+# sum_nums= 6 +4
+# sum_nums = 10, this is the last iteration, the for loop stops
+# then print() starts with the final sum_nums, which is 10. 
+# it prints 
+
+
+# the next for loops is called a NESTED FOR LOOP, 
+# this is when you want to loop for every item in a list, the individual
+# letters of the item e.g.:
+
+words = ["Apple", "Banana", "Car", "Dolphin" ]
+for word in words: 
+	# this loop fetches every word in the list and then ;
+	print ("The following lines will print each letter of " +word)
+	for letter in word:
+		# this loop fetches every letter of a word: 
+		print(letter)
+	print("")	# prints a blank line
+
+
+
+# 4 
+# FOR LOOP with RANGE() function
+# Range

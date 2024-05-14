ccb710ba6e1d4e713579189940c166b9b37a6b82
kaya karakaya <kaya.md@gmail.com>
2024-05-15 01:39:55 +0200

    second grocery list with different code

---
+++ 06_groceries_list.py
@@ -0,0 +1,22 @@
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter food to buy, q to quit. ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f" Enter the price of a {food}: $ " ))
+        foods.append(food)
+        prices.append(price)
+
+print( " =====  MY CART =====")
+
+for food in foods:
+    print(food)
+   
+for price in prices:
+    total += price
+
+print(f" Your total is; ${total}")

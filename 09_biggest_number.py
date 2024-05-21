b127f61e1194e261cbbb090676c67813d753f075
Kaya Karakaya <kaya.md@gmail.com>
2024-05-21 22:50:07 +0200

    added >= instead of > ; added int()

--- 09_biggest_number.py
+++ 09_biggest_number.py
@@ -2,16 +2,16 @@ x = 0
 y = 0
 z = 0
 
-x = input(" Give me 3 numbers, and I will define the biggest: Give me the first number:  ")
+x = int(input("Enter 3 numbers, and I will return the highest: Give me the first number:  "))
 
-y = input("Give me the 2nd number:   ")
-z = input("Give me the 3rd number:   ")
+y = int(input("Enter 2nd number:   "))
+z = int(input("Enter 3rd number:   "))
 
-print(" your numbers are ", x, y, z)
+print(" Your numbers are ", x, y, z)
 
-if (x > y) and (x > z):
+if (x >= y) and (x >= z):
     print(x, "is the biggest")
-elif (y > z) and (y > x):
+elif (y >= x) and (y >= z):
     print(y, "is the biggest")
-elif (z > x) and (z > y ): 
+elif (z >= x) and (z >= y ): 
     print(z, " is the biggest number")
